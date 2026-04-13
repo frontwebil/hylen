24f7bca4e46fac94243e7166f1d068db848fca7d
Illya Sukhomlyn <gollpfd@gmail.com>
2026-04-13 17:58:11 +0300

    add lines

---
+++ src/Components/Lines/UnderProductsLines/UnderProductsLines.tsx
@@ -0,0 +1,20 @@
+import "./style.css";
+
+export function UnderProductsLines() {
+  return (
+    <div className="UnderProductsLines">
+      <div className="UnderProductsLines-line-top">
+        <div className="container">
+          <span className="UnderProductsLines-line-top-left"></span>
+          <span className="UnderProductsLines-line-top-right"></span>
+        </div>
+      </div>
+      <div className="UnderProductsLines-line-bottom">
+        <div className="container">
+          <span className="UnderProductsLines-line-bottom-left"></span>
+          <span className="UnderProductsLines-line-bottom-right"></span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import { Header } from "@/Components/Layout/Header/Header";
 import { LoadingScreen } from "@/Components/LoadingScreen/LoadingScreen";
 import { Products } from "@/Components/Products/Products";
 import { UnderHeroLines } from "@/Components/Lines/UnderHeroLines/UnderHeroLines";
+import { UnderProductsLines } from "@/Components/Lines/UnderProductsLines/UnderProductsLines";
 
 export default function Home() {
   return (
     <>
-      <LoadingScreen />
+      {/* <LoadingScreen /> */}
       <Header />
       <Hero />
       <UnderHeroLines />
       <Products />
+      <UnderProductsLines />
     </>
   );
 }
