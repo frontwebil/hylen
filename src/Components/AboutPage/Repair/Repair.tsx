6ad3a3b630050fb6e6acb038fe6cbfa878fc7621
Illya Sukhomlyn <gollpfd@gmail.com>
2026-04-24 19:10:05 +0300

    update

--- src/Components/AboutPage/Repair/Repair.tsx
+++ src/Components/AboutPage/Repair/Repair.tsx
@@ -13,13 +13,11 @@ export function Repair() {
           <div className="container">
             <span className="UnderByroLines-bottom-left-line"></span>
             <span className="UnderByroLines-bottom-middle-line"></span>
-            <span className="UnderByroLines-bottom-right-line"></span>
           </div>
         </div>
         <div className="UnderByroLines-top">
           <div className="container">
             <span className="UnderByroLines-top-left-line"></span>
-            <span className="UnderByroLines-top-right-line"></span>
           </div>
         </div>
       </section>
