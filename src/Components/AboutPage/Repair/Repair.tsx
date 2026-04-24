3e572b6ac7523c455d033f2d9ee0503f5ed9ac7a
Illya Sukhomlyn <gollpfd@gmail.com>
2026-04-24 19:09:25 +0300

    update

--- src/Components/AboutPage/Repair/Repair.tsx
+++ src/Components/AboutPage/Repair/Repair.tsx
@@ -7,6 +7,22 @@ export function Repair() {
   const width = useWindowWidth();
   return (
     <>
+      <section className="UnderByroLines">
+        {" "}
+        <div className="UnderByroLines-bottom">
+          <div className="container">
+            <span className="UnderByroLines-bottom-left-line"></span>
+            <span className="UnderByroLines-bottom-middle-line"></span>
+            <span className="UnderByroLines-bottom-right-line"></span>
+          </div>
+        </div>
+        <div className="UnderByroLines-top">
+          <div className="container">
+            <span className="UnderByroLines-top-left-line"></span>
+            <span className="UnderByroLines-top-right-line"></span>
+          </div>
+        </div>
+      </section>
       <section className="repair">
         <div className="repair-title">Ремонти – друге життя вашої техніки</div>
         {width && width <= 1000 && (
